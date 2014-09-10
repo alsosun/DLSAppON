31bae23e0672ea5989d65d815531f511bd898860
alsosun <cory.antonini@gmail.com>
2014-09-09 20:58:42 -0600

    new vid link

--- js/myjs.js
+++ js/myjs.js
@@ -130,7 +130,7 @@ $(document).ready(function () {
             if (rubricnm == $(this).text()) {
                 $("#popmessage").html("You already have a rubric \n by that name. Try again.");
                 $("#errorpop").popup("open");
-                event.preventDefault();
+                //event.preventDefault();
                 return false;
             };
         });
@@ -1076,7 +1076,7 @@ $(document).ready(function () {
             var size = scale(497, 298, 15, 1),
                 w = size.width,
                 h = size.height,
-                markup = " <iframe src='http://www.youtube.com/embed/swbeVB3lu8I' width='" + w + "' height='" + h + "'  seamless></iframe>";
+                markup = " <iframe src='http://www.youtube.com/embed/I_K20uIwMz8'  width='" + w + "' height='" + h + "'  seamless></iframe>";
 
             $("#popupVideo-1").html(markup);
             //$("#popupVideo-1 iframe")
